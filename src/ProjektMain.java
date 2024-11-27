9d233f90a348b0bf335c5f218862edbb6470b8ab
pklambau <pklambau@htl-steyr.ac.at>
2024-11-27 09:08:57 +0100

    Fertig

--- src/ProjektMain.java
+++ src/ProjektMain.java
@@ -1,6 +1,8 @@
 class ProjektMain{
     public static void main(String[] args){
         Pklambau pklambau = new Pklambau();
+        Jeberhar jeberhar = new Jeberhar();
         pklambau.tell();
+        jeberhar.tell();
     }
 }
